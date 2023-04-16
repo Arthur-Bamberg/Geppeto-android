22c80f9595065ce7cd24aac4e175b42b1791d68b
Arthur-Bamberg <bamberguisses@gmail.com>
2023-04-16 11:27:36 -0300

    Change folder

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function TextButton() {
+  return <Text>Click me!</Text>;
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
+      <TextButton />
     </View>
   );
 }
