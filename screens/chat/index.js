59cf830869b7731c3ab0b658c1140b908c46523c
Arthur-Bamberg <bamberguisses@gmail.com>
2023-05-17 17:26:53 -0300

    feat: Improving ChatScreen to use messages.json

--- screens/chat/index.js
+++ screens/chat/index.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 export const ChatScreen = () => {
+    const messagesMock = require("../../messages.json");
+
+    const [inputMessage, setInputMessage] = useState('');
+    const [messages, setMessages] = useState(messagesMock);
+
+    const sendMessage = () => {
+        if (inputMessage.trim() === '') return;
+
+        const newMessage = {
+            content: inputMessage.trim(),
+            sender: 'sent',
+        };
+
+        setMessages([...messages, newMessage]);
+        setInputMessage('');
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.headerText}>Geppeto Assistant</Text>
             </View>
             <View style={styles.chatContainer}>
-                <View style={[styles.messageBubble, styles.sentMessage]}>
-                    <Text style={styles.messageText}>Hello!</Text>
-                </View>
-                <View style={[styles.messageBubble, styles.receivedMessage]}>
-                    <Text style={styles.messageText}>Hi there!</Text>
-                </View>
+                {messages.map((message, index) => (
+                    <View
+                        key={index}
+                        style={[
+                            styles.messageBubble,
+                            message.sender === 'sent' ? styles.sentMessage : styles.receivedMessage,
+                        ]}
+                    >
+                        <Text style={styles.messageText}>{message.content}</Text>
+                    </View>
+                ))}
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Type a message..."
+                    value={inputMessage}
+                    onChangeText={setInputMessage}
+                />
+                <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
+                    <Text style={styles.sendButtonText}>Send</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
